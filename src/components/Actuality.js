dfce67e569bb1e03e8cda6b343cb0eff24a02688
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-03-03 10:55:41 +0100

    lenght

--- src/components/Actuality.js
+++ src/components/Actuality.js
@@ -5,15 +5,16 @@ import { useHistory } from "react-router-dom";
 import './Actuality.css'
 
 
+
 export default function Actuality() {
     const [actuality, setActuality] = useState([''])
 
     let history = useHistory('')
-
+    const actuUrl = "https://min-api.cryptocompare.com/data/v2/news/"
     useEffect(()=> {
        try{
         const fetch = async () => {
-            const req = await axios.get("https://min-api.cryptocompare.com/data/v2/news/")
+            const req = await axios.get(actuUrl)
             setActuality(req.data.Data)
         }
         fetch()
@@ -29,17 +30,18 @@ export default function Actuality() {
                 <div className="actuality">
                     {actuality.map(actual => 
                     <div className="MainActuality">
-                        
-                        <div className="left">
-                            <img className="imgActuality" src={actual.imageurl} alt="salut"/>
-                        </div>    
+                    
                         <div className="right">
-                        <a className="TitleActuality" href={actual.url} alt="_blanck" >{actual.title}</a>
+                            <div className="topAct">
+                                <img className="imgActuality" src={actual.imageurl} alt="salut"/>
+                                <a className="TitleActuality" href={actual.url} rel="noreferrer" target="_blank" >{actual.title}</a>
+                            </div>
+                       
                             <div className="infoActuality">
                                 {/* <p>Catégorie : {actual.categories}</p> */}
                                 <p className="SourceActuality">{actual.source} - {actual.published_on}</p>
                             </div>
-                            <p className="Resume">{actual.body > 100 ? actual.body.substring(0,200) + '...' : actual.body }</p>
+                            <p className="Resume">{actual.body ? actual.body.substring(0,150) + '...' : "No resume" }</p>
                             
                         </div>
                     </div>
